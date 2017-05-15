86c2a7ad79699d8053cdc991e2ef6f6f742c28cd
sasquach <warui@poczta.fm>
2017-05-15 10:08:15 +0200

    markdown custom renderer part-2

--- src/Markdown.js
+++ src/Markdown.js
@@ -15,49 +15,63 @@ class Markdown {
     this.marked = marked;
     this.renderer = new marked.Renderer();
 
-    // this.renderer.code = function(code, lang, escaped) {
-    //   if (this.options.highlight) {
-    //     var out = this.options.highlight(code, lang);
-    //     if (out != null && out !== code) {
-    //       escaped = true;
-    //       code = out;
-    //     }
-    //   }
-    //
-    //   if (!lang) {
-    //     return '<pre><code>'
-    //       + (escaped ? code : escape(code, true))
-    //       + '\n</code></pre>';
-    //   }
-    //
-    //   return '<pre><code class="'
-    //     + this.options.langPrefix
-    //     + escape(lang, true)
-    //     + '">'
-    //     + (escaped ? code : escape(code, true))
-    //     + '\n</code></pre>\n';
-    // };
-    // this.renderer.blockquote = (quote) => `<div class="SG-blockquote">${quote}</div>`;
+    function escapeHTML(text) {
+      return text.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+    }
+
+    this.renderer.code = function(code, lang, escaped) {
+      if (this.options.highlight) {
+        const out = this.options.highlight(code, lang);
+        if (out != null && out !== code) {
+          escaped = true;
+          code = out;
+        }
+      }
+
+      if (!lang) {
+        return '<pre class="SG-markdown-code"><code>'
+          + (escaped ? code : escapeHTML(code))
+          + '\n</code></pre>';
+      }
+
+      return '<pre class="SG-markdown-code"><code class="'
+        + this.options.langPrefix
+        + escapeHTML(lang)
+        + '">'
+        + (escaped ? code : escapeHTML(code))
+        + '\n</code></pre>\n';
+    };
+    this.renderer.blockquote = (quote) => `<div class="SG-blockquote">${quote}</div>`;
     this.renderer.heading = (text, level) => `<div class="SG-h${level}">${text}</div>`;
     this.renderer.hr = () => '<div class="SG-hr"></div>';
     this.renderer.list = function(body, ordered) {
       const type = ordered ? 'ol' : 'ul';
-      //return '<' + type + '>\n' + body + '</' + type + '>\n';
       return `<div class="SG-${type}">${body}</div>`;
     };
     this.renderer.listitem = (text) => `<div class="SG-li">${text}</div>`;
     this.renderer.paragraph = (text) => `<div class="SG-p">${text}</div>`;
-    //this.renderer.table = (header, body) => ``;
-    //this.renderer.tablerow = (content) => ``;
-    //this.renderer.tablecell = (content, flags) => ``;
+    this.renderer.table = function(header, body) {
+      return `
+<div class="SG-mtable">
+  <div class="SG-mthead">
+    ${header}
+  </div>
+  <div class="SG-mtbody">
+    ${body}
+  </div>
+</div>`;
+    };
+    this.renderer.tablerow = (content) => `<div class="SG-mtr">${content}</div>`;
+    this.renderer.tablecell = function (content, flags) {
+      const type = flags.header ? 'th' : 'td';
+      const tag = flags.align ? `<div class="SG-m${type} SG-m${type}--right">` : `<div class="SG-m${type}">`;
+      return `${tag}${content}</div>`;
+    };
 
     this.renderer.strong = (text) => `<span class="SG-strong">${text}</span>`;
     this.renderer.em = (text) => `<span class="SG-i">${text}</span>`;
     this.renderer.codespan = (code) => `<span class="SG-code">${code}</span>`;
-    //this.renderer.br = () => ``;
     this.renderer.del = (text) => `<span class="SG-del">${text}</span>`;
-    //this.renderer.link = (href, title, text) => ``;
-    //this.renderer.image = (href, title, text) => ``;
   }
 
   render(str) {
